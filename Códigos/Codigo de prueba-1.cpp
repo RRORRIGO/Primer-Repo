3629115c8bc9ebcf50d634176c3ce7439ddadc96
Rodrigo <jamirvac@gmail.com>
2024-06-10 20:03:24 -0500

    trabajo

--- Códigos/Codigo de prueba-1.cpp
+++ Códigos/Codigo de prueba-1.cpp
@@ -1,5 +1,19 @@
 #include <iostream>
 using namespace std;
+void wallis(float n, char p){
+    int numerador,denominador;
+    float producto=1.0;
+    if(n>0.5){
+    }else{
+        if(p='a'){
+            for(int i=1;producto>n;i++){
+                producto*=(i*2-1)/(2*i);
+                
+ 
+            }
+        }
+    }
+}
 int main(){
     int a;
     int n=1;
